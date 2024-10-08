2535382f0fee77095d7c52f6dcd39fcb9648b734
Marcio Alemão <50804131+marcio1002@users.noreply.github.com>
2024-10-08 16:55:16 -0300

    Update profile.ps1 with PS 7 and PS 5

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,5 @@
+## POWERSHELL 7
+
 #Configuration Autocompletion keys
 Import-Module PSReadLine -RequiredVersion 2.3.5
 Import-Module TrustedPlatformModule
@@ -88,3 +90,69 @@ function prompt {
 
 # User Alias
 . $PSScriptRoot\Modules\user-aliases\alias.ps1
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# POWERSHELL 5
+#Configuration Autocompletion keys
+Import-Module PSReadLine -RequiredVersion 2.3.6
+Import-Module TrustedPlatformModule
+
+Set-PSReadLineOption -ContinuationPrompt "$indicator "
+Set-PSReadLineOption -HistoryNoDuplicates:$True
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$True
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -ShowToolTips:$True
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -ViModeIndicator Script
+Set-PSReadLineOption -BellStyle Visual
+Set-PSReadLineOption -Colors @{
+    ContinuationPrompt = '#ffffff'
+    Emphasis           = '#3ABAD0'
+    ERROR              = '#FF3B18'
+    Selection          = '#64FF59'
+    Default            = '#ffffff'
+    Comment            = '#ffffff'
+    Keyword            = '#9D2CFF'
+    String             = '#FFB230'
+    Operator           = '#2FFF9F'
+    Command            = '#64FF59'
+    Variable           = '#A84FFF'
+    Parameter          = '#F8FF39'
+    Type               = '#AF0917'
+    Number             = '#CA41FF'
+    Member             = '#818CFF'
+    InlinePrediction   = '#F8FF39'
+}
+
+# Autocompletion for arrow keys
+Remove-PSReadLineKeyHandler -Key Ctrl+C
+Remove-PSReadLineKeyHandler -Key Shift+Enter
+
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Ctrl+c, Ctrl+C -Function CancelLine
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+a, Ctrl+Shift+A -Function SelectAll
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+c, Ctrl+Shift+C -Function Copy
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+v, Ctrl+Shift+V -Function Paste 
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+l, Ctrl+Shift+L -Function RevertLine
+Set-PSReadLineKeyHandler -Chord Ctrl+z, Ctrl+Z -Function Undo
+Set-PSReadLineKeyHandler -Chord Ctrl+LeftArrow -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+RightArrow -Function NextWord
+Set-PSReadLineKeyHandler -Chord Ctrl+Shift+Backspace -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Chord Ctrl+Enter -Function AddLine
+
+
+# Customização do prompt
+function prompt {
+    $pathCurrent = Get-Location | Split-Path -Leaf
+
+    $customPrompt += "$pathCurrent > "
+
+    "$customPrompt"
+}
+
+
+# User Alias
+# . $PSScriptRoot\Modules\user-aliases\alias.ps1
